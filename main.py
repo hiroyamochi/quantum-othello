6b09345c775d9a29854ef3c2ad286a21929f9201
hiroyamochi <hirodora13@gmail.com>
2024-03-20 00:37:07 +0900

    update text displaying & modify auto playing

--- main.py
+++ main.py
@@ -68,12 +68,12 @@ class Othello:
     return True
   
   # 量子力学的な要素を追加
-  # 70%の確率で石が裏返る
+  # 30%の確率で石が裏返る
   def schroedinger(self):
     for row in range(self.size):
       for col in range(self.size):
         if self.board[row][col] != ' ':
-          if random.random() < 0.7:
+          if random.random() < 0.3:
             self.board[row][col] = self.opponent()
 
   def count_score(self):
@@ -83,15 +83,22 @@ class Othello:
   
   def result(self):
     print('ゲーム終了!')
-    othello.print_board()
+    self.print_board()
+    count_x, count_o = self.count_score()
+    print(f'X: {count_x}、O: {count_o}')
+
+    print('')
+    print('(=^-^)ﾉ')
 
     print('シュレディンガーの猫がボードを通過しました...')
-    othello.schroedinger()
+    self.schroedinger()
+
+    print('')
 
     print('最終結果:')
-    othello.print_board()
+    self.print_board()
 
-    count_x, count_o = othello.count_score()
+    count_x, count_o = self.count_score()
     print(f'X: {count_x}、O: {count_o}')
     if count_x > count_o:
         print('Xの勝利です!')
@@ -100,42 +107,43 @@ class Othello:
     else:
         print('引き分けです！')
 
-# ゲームのメイン部分
-size = input('ボードのサイズを入力してください (4-16): ')
-if int(size) < 4 or int(size) > 16:
-    print('サイズが無効です。デフォルトのサイズ8を使用します。')
-    size = 8
-othello = Othello(int(size))
-while True:
-    othello.print_board()
-    print('-----------------------------------')
-    print(f'{othello.current_player}のターンです。')
+if __name__ == '__main__':
+  # ゲームのメイン部分
+  size = input('ボードのサイズを入力してください (4-16): ')
+  if int(size) < 4 or int(size) > 16:
+      print('サイズが無効です。デフォルトのサイズ8を使用します。')
+      size = 8
+  othello = Othello(int(size))
+  while True:
+      othello.print_board()
+      print('-----------------------------------')
+      print(f'{othello.current_player}のターンです。')
 
-    # 有効な手があるかを確認
-    valid_moves_available = False
-    for row in range(8):
-        for col in range(8):
-            if othello.is_valid_move(row, col):
-                valid_moves_available = True
-                break
-        if valid_moves_available: # 1列ごとに有効な手があればループを抜ける
-            break
-    if not valid_moves_available:
-        print(f'{othello.current_player}の手がありません。パスします。')
-        break
-    
-    # 入力を受け取る
-    while True:
-        try:
-            row = int(input('行を選択してください (0-7): '))
-            col = int(input('列を選択してください (0-7): '))
-            if othello.is_valid_move(row, col):
-                othello.make_move(row, col)
-                break
-            else:
-                print('無効な動きです。再試行してください。')
-        except ValueError:
-            print('整数を入力してください。')
+      # 有効な手があるかを確認
+      valid_moves_available = False
+      for row in range(8):
+          for col in range(8):
+              if othello.is_valid_move(row, col):
+                  valid_moves_available = True
+                  break
+          if valid_moves_available: # 1列ごとに有効な手があればループを抜ける
+              break
+      if not valid_moves_available:
+          print(f'{othello.current_player}の手がありません。パスします。')
+          break
+      
+      # 入力を受け取る
+      while True:
+          try:
+              row = int(input('行を選択してください (0-7): '))
+              col = int(input('列を選択してください (0-7): '))
+              if othello.is_valid_move(row, col):
+                  othello.make_move(row, col)
+                  break
+              else:
+                  print('無効な動きです。再試行してください。')
+          except ValueError:
+              print('整数を入力してください。')
 
-# ゲーム終了
-othello.result()
+  # ゲーム終了
+  othello.result()
